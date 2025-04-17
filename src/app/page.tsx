42b1ac684d419ce3b1f50b158e4c7049ff85a7b9
Md Adil <md-adil@live.com>
2025-04-17 19:38:32 +0530

    fix title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,39 +10,33 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <div className="bg-blue-950">
- <Hero />
+      <Hero />
 
-<section className="my-5">
-<Challenges />
+      <section className="my-5">
+        <Challenges />
+      </section>
+      <section className="my-5">
+        <WhyUsSection />
+      </section>
 
-</section>
-<section className="my-5">
-<WhyUsSection />
+      <section className="my-5">
+        <ServicesSection />
+      </section>
 
-</section>
+      <section className="my-5">
+        <LiveHotDeals />
+      </section>
 
-<section className="my-5">
-<ServicesSection />
+      <section className="my-5">
+        <TestimonialSlider />
+      </section>
+      <section className="my-5">
+        <BlogSection />
+      </section>
 
-</section>
-
-<section className="my-5">
-<LiveHotDeals />
-</section>
-
-<section className="my-5">
-<TestimonialSlider />
-</section>
-<section className="my-5">
-<BlogSection />
-</section>
-
-<section className="my-5">
-<Footer />
-</section>
+      <section className="my-5">
+        <Footer />
+      </section>
     </div>
-   
-    
-   
   );
 }
