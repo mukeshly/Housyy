67a0acfd26e327d9ab5c8f13c4a492445e256f9b
Mukesh Jha <mukeshjha5661@gmail.com>
2026-04-02 04:47:58 +0530

    Fix Netlify asset paths

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: isProd ? "export" : undefined,
-  basePath: isProd ? "/Housy" : undefined,
   images: {
     unoptimized: true,
   },
